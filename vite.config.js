d044336035e976a1307ff2c82c3e791bfc59b512
abecerraguz <alejandro.becerra@publimetro.cl>
2023-05-10 00:27:35 -0400

    Se guardan los cambios

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,32 @@
-import legacy from '@vitejs/plugin-legacy'
+// import legacy from '@vitejs/plugin-legacy'
 
-export default {
-  // root: './src',
-  // build: {
-  //   manifest: true,
-  // },
-  // base: '/VITE-FINAL/',
-  plugins: [
-    legacy({
+// export default {
+//   // root: './src',
+//   // build: {
+//   //   manifest: true,
+//   // },
+//   // base: '/VITE-FINAL/',
+//   plugins: [
+//     legacy({
+//       polyfills: ['es.promise.finally', 'es/map', 'es/set'],
+//       modernPolyfills: ['es.promise.finally'],
+//     })
+//   ]
+// }
+
+
+import legacy from '@vitejs/plugin-legacy';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [
+		legacy({
       polyfills: ['es.promise.finally', 'es/map', 'es/set'],
-      modernPolyfills: ['es.promise.finally'],
-    })
-  ]
-}
-
-
-// import legacy from '@vitejs/plugin-legacy';
-// import { defineConfig } from 'vite';
-
-// export default defineConfig({
-// 	plugins: [
-// 		legacy({
-// 			targets: ['defaults', 'not IE 11'],
-// 		}),
-// 	],
-// 	build: {
-// 		manifest: true,
-// 	},
-// 	base: '/vite-project/',
-// });
+      modernPolyfills: ['es.promise.finally']
+		}),
+	],
+	build: {
+		manifest: true,
+	},
+	base: '/VITE-FINAL/',
+});

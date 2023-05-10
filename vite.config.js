5c73d8ca5c347e5788d6dea05d23603e516257e4
abecerraguz <alejandro.becerra@publimetro.cl>
2023-05-09 23:57:36 -0400

    Se guardan los cambios

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,31 @@
 import legacy from '@vitejs/plugin-legacy'
 
 export default {
+  root: './src',
+  build: {
+    manifest: true,
+  },
+  base: '/VITE-FINAL/',
   plugins: [
     legacy({
       polyfills: ['es.promise.finally', 'es/map', 'es/set'],
       modernPolyfills: ['es.promise.finally'],
-    }),
-  ],
+    })
+  ]
 }
+
+
+// import legacy from '@vitejs/plugin-legacy';
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+// 	plugins: [
+// 		legacy({
+// 			targets: ['defaults', 'not IE 11'],
+// 		}),
+// 	],
+// 	build: {
+// 		manifest: true,
+// 	},
+// 	base: '/vite-project/',
+// });
